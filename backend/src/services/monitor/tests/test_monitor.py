c93d3e850b6f2b4bfa8c2905e83450a1f845a0e0
Erick <simasgrilo86@gmail.com>
2026-02-24 01:01:00 -0300

    Add: unit tests and module entries

---
+++ backend/src/services/monitor/tests/test_monitor.py
@@ -0,0 +1,16 @@
+import unittest
+from backend.src.services.monitor.core.monitor import Monitor
+from backend.src.services.database.core.database_mock import DatabaseMock
+from backend.src.services.monitor.models.url import URL
+
+class TestMonitor(unittest.TestCase):
+    """Test class for monitor features"""
+    def setUp(self) -> None:
+        urls = [URL(url="www.google.com"), URL(url="www.uxinnovation.com.br")]
+        self.monitor = Monitor(urls, DatabaseMock(), {})
+        super().setUp()
+
+
+    def test_monitor_url(self):
+        """Basic unit test to monitor a handful of URLs and print the results to the console."""
+        self.monitor.monitor_urls()
